ea86c1f307f4a1e74a288d5b78a2c94bb8e77f7c
Marina Carvalho <marinacarvalho98@proton.me>
2025-10-15 15:25:03 -0400

    Scaffold: Spring Boot app, config, tests

---
+++ src/main/java/io/datalatte/app/DataLatteApplication.java
@@ -0,0 +1,10 @@
+package io.datalatte.app;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLatteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataLatteApplication.class, args);
+    }
+}
